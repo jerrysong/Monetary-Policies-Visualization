b195fb1203238c86746baa2d9c46d9e78197a6c6
jerrysong <songlingyi@gmail.com>
2017-07-04 16:17:41 -0700

    update

--- resources/scripts/csv_processor.py
+++ resources/scripts/csv_processor.py
@@ -1,12 +1,36 @@
-date_list = []
-index_list = []
-with open('../data/Market_and_Economy/209FinalProject_DailyData.csv') as f:
-    for line in f:
-        date, index, _, _ = line.rstrip().split(',')
-        if len(date) == 0:
-            break
-        else:
-            date_list.append(date)
-            index_list.append(float(index))
+import json
 
-print index_list
+date_list = []
+stock_list = []
+bond_list = []
+date_set = set()
+
+with open('../data/sp500.csv') as f:
+    for line in f:
+        date, stock = line.rstrip().split(',')
+        date_list.append(date)
+        date_set.add(date)
+        stock_list.append(float(stock))
+
+with open('../data/bond.csv') as f:
+    for line in f:
+        date, bond = line.rstrip().split(',')
+        if date not in date_set:
+            continue
+        else:
+            bond_list.append(float(bond))
+
+# The entries in each output dataset should be equal, even if entries number from the raw data are different.
+assert len(date_list) == len(stock_list) == len(bond_list)
+print "Process %d entries in total." % (len(date_list))
+
+dict_data = {
+    "xData": date_list,
+    "datasets": [
+        {"name": "S&P 500", "data": stock_list, "unit": "", "type": "area", "valueDecimals": 2},
+        {"name": "Bond", "data": bond_list, "unit": "", "type": "line", "valueDecimals": 2}
+    ]
+}
+
+with open('../data/stock_bond_data.json', 'w') as f:
+    json.dump(dict_data, f)
